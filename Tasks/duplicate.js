100e8ec8eb277c1f3c0523db33d0f478b80fc750
ArtemVasylevych <artemka0314@gmail.com>
2025-01-14 21:33:39 +0200

    changed n -> number and const||let added

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,14 +1,12 @@
 'use strict';
 
-duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
+duplicate = (value, number) => {
+  if (number <= 0) return [];
     res = [];
     for (let i = 0; i < N; i++) {
       res[i] = value;
     }
     return res;
-  }
 };
 
 module.exports = duplicate;
