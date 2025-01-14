4ae5657136e9729cedf848a47c26816574a79e13
ArtemVasylevych <artemka0314@gmail.com>
2025-01-14 21:44:22 +0200

    removed: let res... and for....changed res -> Array(number....

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -2,11 +2,7 @@
 
 const duplicate = (value, number) => {
   if (number <= 0) return [];
-    let res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
-    }
-    return res;
+    return Array(number).fill(value);
 };
 
 module.exports = duplicate;
