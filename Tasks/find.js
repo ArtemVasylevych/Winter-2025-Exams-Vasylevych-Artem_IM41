53c2ea14997cedcedb20a94b8a23fd54dbdf6f6b
ArtemVasylevych <artemka0314@gmail.com>
2025-01-14 21:10:28 +0200

    changed obj -> object

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const findObjByValue = (object, value) => {
-  const names = Object.keys(obj);
+  const names = Object.keys(object);
   for (const name of names) {
     if (object[name] === value) {
       return name;
