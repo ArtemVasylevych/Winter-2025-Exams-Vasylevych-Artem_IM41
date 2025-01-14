ff71570afa6294174dce0237513d3c0dc30c467f
ArtemVasylevych <artemka0314@gmail.com>
2025-01-14 20:30:51 +0200

    forEach ->for...of and objValues added

--- Tasks/count.js
+++ Tasks/count.js
@@ -2,13 +2,12 @@
 
 const count = (obj) => {
   let sum = 0;
-  const keys = Object.keys(obj);
-  keys.forEach((key) => {
-    const value = obj[key];
+  const objValues = Object.values(obj);
+  for (const value of objValues) {
     if (typeof value === 'number') {
       sum += value;
-    };
-  });
+    }
+  }
   return sum;
 };
 
